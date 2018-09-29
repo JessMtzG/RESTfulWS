b9f5a4d653c0f8ea7a3f0a6947089ecb0bc54137
JessMtzG <35821866+JessMtzG@users.noreply.github.com>
2018-09-28 19:25:09 -0500

    Commit: Recurso file

---
+++ CS_13304/src/main/java/me/jmll/utm/model/OptionsDoc.java
@@ -0,0 +1,18 @@
+package me.jmll.utm.model;
+
+import java.util.HashMap;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.springframework.http.HttpMethod;
+
+@XmlRootElement
+public class OptionsDoc {
+
+	 HashMap<HttpMethod, String> options;
+	public OptionsDoc(){
+		 options = new HashMap<HttpMethod, String>();
+	 }
+	 public void addOption(HttpMethod option, String description){
+		 options.put(option, description);
+	 }
+
+}
